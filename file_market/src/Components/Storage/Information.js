70c48e19790f18991e5018a59ae4d3d13f71b7cb
nahyun0121 <knh4769@khu.ac.kr>
2023-05-11 02:17:55 +0900

    Create basic configuration for Storage

---
+++ file_market/src/Components/Storage/Information.js
@@ -0,0 +1,19 @@
+import styles from "../../assets/css/Storage/Information.module.css";
+const FavoriteFile = () => {
+  return <div>favoritefile</div>;
+};
+
+const Information = () => {
+  return (
+    <div className={styles.information}>
+      <div>
+        <FavoriteFile></FavoriteFile>
+      </div>
+      <br></br>
+      <br></br>
+      informations..
+    </div>
+  );
+};
+
+export default Information;
